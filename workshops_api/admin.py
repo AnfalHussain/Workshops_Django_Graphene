7ea13f340927ad9f4d6e9b4b418b840ccba55132
AnfalHussain <anfalae95@gmail.com>
2020-09-15 20:39:39 +0300

    setup

---
+++ workshops_api/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Profile, Workshop, Registration, Cart
+
+admin.site.register(Profile)
+admin.site.register(Workshop)
+admin.site.register(Registration)
+admin.site.register(Cart)
